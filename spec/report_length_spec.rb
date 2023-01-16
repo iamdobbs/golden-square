b6692b8c2e58ac1b99a90bc10ae74ffaf3c84e71
Hayley Dobbs <iamdobbs@icloud.com>
2023-01-16 17:08:51 +0000

    Created report_length.rb and added test

---
+++ spec/report_length_spec.rb
@@ -0,0 +1,8 @@
+require 'report_length'
+
+RSpec.describe "report_length method" do
+  it "returns the length of a string" do
+    result = report_length("horse")
+    expect(result).to eq("This string was 5 characters long.")
+  end
+end
