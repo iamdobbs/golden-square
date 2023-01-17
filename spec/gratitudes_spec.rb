6665cbfcfc3a75e236c0c32685fff7b8c334a7c6
Hayley Dobbs <iamdobbs@icloud.com>
2023-01-17 15:32:45 +0000

    Refactored RSpec gratitudes_spec.rb

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -1,16 +1,19 @@
 require 'gratitudes'
 
 RSpec.describe Gratitudes do
-  it 'adds string to gratitude variable' do
-    first = Gratitudes.new
-    result = first.add('lucozade')
+
+  before(:each) do
+    @test = Gratitudes.new
+  end
+
+  it 'adds string to gratitude array' do
+    result = @test.add('lucozade')
     expect(result).to eq ['lucozade']
   end
 
   it 'concatenates fixed string with array input' do
-    second = Gratitudes.new
-    second.add('lucozade')
-    result = second.format
+    @test.add('lucozade')
+    result = @test.format
     expect(result).to eq "Be grateful for: lucozade"
   end  
 end
